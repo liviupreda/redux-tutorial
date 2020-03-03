c2b04453e7d540919a9e508c940d65f8e4456ebf
Liviu Preda <liviupreda@gmail.com>
2020-03-03 16:24:39 +0200

    add setFilter to component props, filter is working now

--- src/components/VisibilityFilters.js
+++ src/components/VisibilityFilters.js
@@ -4,7 +4,7 @@ import { VISIBILITY_FILTERS } from "../constants";
 import { connect } from "react-redux";
 import { setFilter } from "../redux/actions";
 
-const VisibilityFilters = ({ activeFilter }) => {
+const VisibilityFilters = ({ activeFilter, setFilter }) => {
   return (
     <div className="visibility-filters">
       {Object.keys(VISIBILITY_FILTERS).map(filterKey => {
@@ -16,7 +16,11 @@ const VisibilityFilters = ({ activeFilter }) => {
               "filter",
               currentFilter === activeFilter && "filter--active"
             )}
-            onClick={() => {} /** waiting for setFilter handler*/}
+            onClick={
+              () => {
+                setFilter(currentFilter);
+              } /** waiting for setFilter handler*/
+            }
           >
             {currentFilter}
           </span>
